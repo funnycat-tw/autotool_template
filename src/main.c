220168b2b2beecbc93cdc02ef5f4c56e07c500ac
Jdoe Chen <funnycat@gmail.com>
2014-11-04 14:13:11 +0800

    modify src/main.c to include include/package.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "package.h"
 
 int main(int argc, char *argv[]) {
     printf("This is main.c\n");
+#ifdef __MY_PACKAGE_H__
+    printf("mypackage header included.\n");
+#endif
 } /* main */
 
